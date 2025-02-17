3cbbacecb5818a788663ecf629b7e35daf139061
manofsteeljj <justinjoseph.sanchez@lorma.edu>
2025-02-17 21:50:54 +0800

    Woring para Exam

---
+++ database/migrations/2025_02_09_111249_create_incidents_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncidentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->string('type');
+            $table->string('evidence_path')->nullable();
+            $table->enum('status', ['Pending', 'In Progress', 'Resolved'])->default('Pending');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+}
